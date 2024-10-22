7022c50789d5929fc0813583c0d009f7cc968557
Isaacc20 <isaacoluwadarasimi002@gmail.com>
2024-10-23 00:39:23 +0100

    input component

--- src/components/ui/organisms/forms/signUpForm/SignUpPage1.tsx
+++ src/components/ui/organisms/forms/signUpForm/SignUpPage1.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Input from '../../../atoms/forms/Input'
+import {FiFlag, FiMail} from 'react-icons/fi'
 
 const SignUpPage1: React.FC = () => {
   return (
@@ -6,6 +8,7 @@ const SignUpPage1: React.FC = () => {
       <div className="d-flex flex-column">
         <h1>Create your account</h1>
         <span>Launch your program in no time just by creating an account with us.</span>
+        <Input inpType='select' onchange={(e:any)=>console.log(e.target.value)} placeHolder='Enter your name' options={[{value: "Nigeria", content: "Nigeria"}]} icon={<FiFlag className='icon' />} />
       </div>
     </form>
   )
