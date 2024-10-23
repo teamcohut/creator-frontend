6ada4ab547ff75eb986791832cf2fd521fe2b254
Emerald-A <adegbitedavid01@gmail.com>
2024-10-23 21:11:59 +0100

    login/forgot-password

--- src/components/templates/AuthTemplate/index.tsx
+++ src/components/templates/AuthTemplate/index.tsx
@@ -6,17 +6,17 @@ const AuthTemplate: FC<IAuthTemplate> = ({children, title}) => {
   return (
     <>
         <div className='container-fluid justify-between form-template px-4 py-6 p-md-5'>
-        <div className='row justify-content-between'>
-          <div className='col-6 d-flex flex-column justify-content-between'>
-            <Link to={'/'}>
-              <img src={'/assets/images/Cohut.png'} alt={'Cohut Logo'} />
-            </Link>
-            <h1 className='mt-auto text-white'>{title}</h1>
+          <div className='row justify-content-between'>
+            <div className='col-6 d-flex flex-column justify-content-between'>
+              <Link to={'/'}>
+                <img src={'/assets/images/Cohut.png'} alt={'Cohut Logo'} />
+              </Link>
+              <h1 className='mt-auto text-white'>{title}</h1>
+            </div>
+            <div className='col-6 d-flex align-items-center justify-content-center'> 
+              {children}
+            </div>
           </div>
-          <div className='col-6'> 
-            {children}
-          </div>
-        </div>
         </div>
     </>
   )
