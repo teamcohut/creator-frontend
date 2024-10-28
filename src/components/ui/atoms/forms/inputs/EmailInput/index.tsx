295ef6d3deeb631b0a7965c4e6f51fc6c0338b67
Emerald-A <adegbitedavid01@gmail.com>
2024-10-28 13:19:42 +0100

    form validation

--- src/components/ui/atoms/forms/inputs/EmailInput/index.tsx
+++ src/components/ui/atoms/forms/inputs/EmailInput/index.tsx
@@ -1,23 +1,47 @@
-import { FC } from 'react'
-import { IInput } from '../../../../../../@types/input.interface'
+import { FC, useState } from 'react';
+import { IInput } from '../../../../../../@types/input.interface';
 import { FiMail } from "react-icons/fi";
-import '../index.css'
+import '../index.css';
 
 const EmailInput: FC<IInput> = (props) => {
-    const { label, id, placeHolder, onchange, icon } = props
-  return (
-    <>
-        <div className="input-cont d-flex flex-column align-items-stretch w-100 gap-2">
-          {label && 
-            <label className='manrope-600 fs-body' htmlFor={id}>{label}</label>
-          }
-          <div className='input-div d-flex align-items-center gap-2 rounded-pill px-3'>
-            {icon? icon: <FiMail className='icon' />}
-             <input id={id} className="input bg-transparent w-100 h-100 border-none" type="email" placeholder={placeHolder} onChange={onchange} />
-          </div>
-        </div>
-    </>
-  )
-}
+    const { label, id, placeHolder, onchange, icon } = props;
+    const [isInvalid, setIsInvalid] = useState(false);
 
-export default EmailInput
+    const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        onchange?.(e); 
+        setIsInvalid(false); 
+    };
+
+    const handleOnInvalid = (e: React.InvalidEvent<HTMLInputElement>) => {
+        e.preventDefault(); 
+        setIsInvalid(true); 
+    };
+
+    return (
+        <div className="input-cont d-flex flex-column align-items-stretch w-100 gap-2">
+            {label && 
+                <label className="manrope-600 fs-body" htmlFor={id}>{label}</label>
+            }
+            <div className="input-div d-flex align-items-center gap-2 rounded-pill px-3">
+                {icon ? icon : <FiMail className="icon" />}
+                <input 
+                    id={id}
+                    className="input bg-transparent w-100 h-100 border-none"
+                    type="email"
+                    placeholder={placeHolder}
+                    onChange={handleOnChange}
+                    onInvalid={handleOnInvalid}
+                    required
+                />
+            </div>
+
+            {isInvalid && (
+                <div className="text-danger fs-caption">
+                    {label} is required and should be a valid email.
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default EmailInput;
--- src/components/ui/atoms/forms/inputs/TextInput/index.tsx
+++ src/components/ui/atoms/forms/inputs/TextInput/index.tsx
@@ -1,20 +1,51 @@
-import React from 'react'
-import { IInput } from "../../../../../../@types/input.interface"
-import "../index.css"
+import React, { useState } from 'react';
+import { IInput } from "../../../../../../@types/input.interface";
+import "../index.css";
 
 const TextInput: React.FC<IInput> = (props) => {
-    const { label, id, icon, placeHolder, onchange } = props
-  return (
-    <>
-        <div className="input-cont d-flex flex-column align-items-stretch w-100 gap-2">
-          {label && <label className='manrope-600 fs-body' htmlFor={id}>{label}</label>}
-          <div className='input-div d-flex align-items-center gap-2 rounded-pill px-3'>
-            {icon && icon}
-            <input id={id} className="input bg-transparent w-100 h-100 border-none" type="text"  placeholder={placeHolder} onChange={onchange}/>
-          </div>
-        </div>
-    </>
-  )
-}
+    const { label, id, icon, placeHolder, onchange } = props;
+    
+    const [isInvalid, setIsInvalid] = useState(false);
 
-export default TextInput
+    const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        onchange?.(e); 
+        setIsInvalid(false); 
+    };
+
+    const handleOnInvalid = (e: React.InvalidEvent<HTMLInputElement>) => {
+        e.preventDefault(); 
+        setIsInvalid(true); 
+    };
+
+    return (
+        <>
+            <div className="input-cont d-flex flex-column align-items-stretch w-100 gap-2">
+                {label && (
+                    <label className='manrope-600 fs-body' htmlFor={id}>
+                        {label}
+                    </label>
+                )}
+                <div className='input-div d-flex align-items-center gap-2 rounded-pill px-3'>
+                    {icon && icon}
+                    <input
+                        id={id}
+                        className="input bg-transparent w-100 h-100 border-none"
+                        type="text"
+                        placeholder={placeHolder}
+                        onChange={handleOnChange}
+                        onInvalid={handleOnInvalid}
+                        required
+                    />
+                </div>
+
+                {isInvalid && (
+                    <p className="text-danger fs-caption">
+                        {label} is required.
+                    </p>
+                )}
+            </div>
+        </>
+    );
+};
+
+export default TextInput;
