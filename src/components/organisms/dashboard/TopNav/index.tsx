be328bde44c7e3eff425aad717b390d6760cf0de
Isaacc20 <isaacoluwadarasimi002@gmail.com>
2024-11-15 09:46:55 +0100

    topnav added

--- src/components/organisms/dashboard/TopNav/index.tsx
+++ src/components/organisms/dashboard/TopNav/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { FiBell } from 'react-icons/fi'
 import SearchInput from '../../../atoms/inputs/SearchInput'
+import './index.css'
 
 const TopNav = () => {
   return (
     <>
-      <div className="top-nav w-100 d-flex justify-centent-between">
-        <SearchInput id='search' onchange={()=>{}} placeHolder='Search for anything' label='' />
-        <div className="d-felx align-items-center gap-2">
-          <button><FiBell /></button>
-          <img src="/assets/images/bootcamp.png" width={30} alt="" />
+      <div className="top-nav w-100 d-flex justify-content-between">
+        <div className='search-div'>
+          <SearchInput id='search' onchange={()=>{}} placeHolder='Search for anything' label='' />
+        </div>
+        <div className="d-flex align-items-center gap-2">
+          <button className='btn'><FiBell className='fs-h3' /></button>
+          <img src="/assets/images/bootcamp.png" className='rounded-circle' width={30} height={30} alt="" />
         </div>
       </div>
     </>
