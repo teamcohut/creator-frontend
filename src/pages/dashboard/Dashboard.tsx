2a5ca9be391b5da4c97b23e8a89c054c70f4c1bb
Isaacc20 <isaacoluwadarasimi002@gmail.com>
2024-12-03 23:04:53 +0100

    dynamic modal redering

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 // import Button from "../../components/atoms/Button";
 // import { FiChevronDown, FiDownload, FiPlus } from "react-icons/fi";
 // import CourseDisplay from "../../components/organisms/dashboard/CourseDisplay";
@@ -13,93 +13,71 @@ import { AuthContext } from "../../context/auth/AuthContext";
 import Modal from "../../components/organisms/dashboard/Modal";
 import ProgramDetail from "../../components/organisms/forms/CustomizeProgram/programdetails";
 import CustomizeProgram from "../../components/organisms/forms/CustomizeProgram/CustomizeProgram";
+import DashBoard from "../../components/organisms/dashboard/MainDashboard/DashBoard";
 
 const Dashboard = () => {
   const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [activeModal, setActiveModal] = useState<TActiveModal>(null);
   const [currentStep, setCurrentStep] = useState<number>(0); // Step-based state
   const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    
+  }, [])
+  
 
   const openModal = () => setModalOpen(true);
   const closeModal = () => {
     setModalOpen(false)
   };
 
-  // Step-based component rendering
-  // const renderStep = () => {
-  //   switch (currentStep) {
-  //     case 0:
-  //       return (
-  //         <SetupProgram
-  //           openModal={() => {
-  //             openModal();
-  //             setCurrentStep(1); // Move to the next step
-  //           }}
-  //         />
-  //       );
-  //     case 1:
-  //       return (
-  //         <Modal open={modalOpen}>
-  //           <ProgramDetail
-  //             onContinue={() => {
-  //               closeModal();
-  //               setCurrentStep(2);
-  //             }}
-  //           />
-  //         </Modal>
-  //       );
-  //     case 2:
-  //       return (
-  //         <Modal open={modalOpen}>
-  //           <CustomizeProgram />
-  //         </Modal>
-  //       );
-  //     default:
-  //       return <div>Unknown step</div>;
-  //   }
-  // };
-
   return (
     <>
-      {/* <DashBoard /> */}
+      <DashBoard />
 
       <SetupProgram
         openModal={() => {
           openModal();
           setCurrentStep(1);
+          setActiveModal('program')
         }}
       />
 
-      <Modal open={modalOpen} setModalOpen={setModalOpen}>
-        {
-          currentStep === 1 ?
-            <ProgramDetail
-              onContinue={() => {
-                setCurrentStep(2);
-              }}
-            /> :
+      {
+        activeModal === 'program'?
+        <Modal open={modalOpen} setModalOpen={setModalOpen}>
+          {
+            currentStep === 1 ?
+              <ProgramDetail
+                onContinue={() => {
+                  setCurrentStep(2);
+                }}
+              /> :
             currentStep === 2 ?
               <CustomizeProgram /> :
               <></>
-        }
-      </Modal>
-
-      {/* {
-          currentStep === 1?
-          <Modal open={modalOpen} setModalOpen={setModalOpen}>
-            <ProgramDetail
-              onContinue={() => {
-                setCurrentStep(2);
-              }}
-            />
-          </Modal>:
-          currentStep === 2?
-          <Modal open={modalOpen} setModalOpen={setModalOpen}>
-            <CustomizeProgram />
-          </Modal>:
-          <></>
-        } */}
+          }
+        </Modal>:
+        activeModal === 'cohort'?
+        <Modal open={modalOpen} setModalOpen={setModalOpen}>
+          {
+            currentStep === 1 ?
+              <ProgramDetail
+                onContinue={() => {
+                  setCurrentStep(2);
+                }}
+              /> :
+            currentStep === 2 ?
+              <CustomizeProgram /> :
+              <></>
+          }
+        </Modal>:
+        <></>
+      }
     </>
   );
 };
+
+type TActiveModal = "program" | "cohort" | null
 
 export default Dashboard;
