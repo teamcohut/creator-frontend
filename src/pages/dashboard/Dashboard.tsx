e262511b6b5df8acc9a78ca054475d8ee223fa22
Isaacc20 <isaacoluwadarasimi002@gmail.com>
2024-12-03 23:11:30 +0100

    revert

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,12 +1,4 @@
 import React, { useContext, useEffect, useState } from "react";
-// import Button from "../../components/atoms/Button";
-// import { FiChevronDown, FiDownload, FiPlus } from "react-icons/fi";
-// import CourseDisplay from "../../components/organisms/dashboard/CourseDisplay";
-// import DeadlineDisplay from "../../components/organisms/dashboard/upcomingDeadline/DeadlineDisplay";
-// import RecentActivity from "../../components/organisms/dashboard/RecentActivity";
-// import Overview from "../../components/organisms/dashboard/Overview";
-// import Header from "../../components/organisms/dashboard/Header";
-// import DashBoard from "../../components/organisms/dashboard/MainDashboard/DashBoard";
 import "./index.css";
 import SetupProgram from "../../components/organisms/dashboard/SetupProgram/SetupProgram";
 import { AuthContext } from "../../context/auth/AuthContext";
@@ -17,7 +9,6 @@ import DashBoard from "../../components/organisms/dashboard/MainDashboard/DashBo
 
 const Dashboard = () => {
   const [modalOpen, setModalOpen] = useState<boolean>(false);
-  const [activeModal, setActiveModal] = useState<TActiveModal>(null);
   const [currentStep, setCurrentStep] = useState<number>(0); // Step-based state
   const { user } = useContext(AuthContext);
 
@@ -39,45 +30,24 @@ const Dashboard = () => {
         openModal={() => {
           openModal();
           setCurrentStep(1);
-          setActiveModal('program')
         }}
       />
 
-      {
-        activeModal === 'program'?
-        <Modal open={modalOpen} setModalOpen={setModalOpen}>
-          {
-            currentStep === 1 ?
-              <ProgramDetail
-                onContinue={() => {
-                  setCurrentStep(2);
-                }}
-              /> :
-            currentStep === 2 ?
-              <CustomizeProgram /> :
-              <></>
-          }
-        </Modal>:
-        activeModal === 'cohort'?
-        <Modal open={modalOpen} setModalOpen={setModalOpen}>
-          {
-            currentStep === 1 ?
-              <ProgramDetail
-                onContinue={() => {
-                  setCurrentStep(2);
-                }}
-              /> :
-            currentStep === 2 ?
-              <CustomizeProgram /> :
-              <></>
-          }
-        </Modal>:
-        <></>
-      }
+      <Modal open={modalOpen} setModalOpen={setModalOpen}>
+        {
+          currentStep === 1 ?
+            <ProgramDetail
+              onContinue={() => {
+                setCurrentStep(2);
+              }}
+            /> :
+          currentStep === 2 ?
+            <CustomizeProgram /> :
+            <></>
+        }
+      </Modal>
     </>
   );
 };
-
-type TActiveModal = "program" | "cohort" | null
 
 export default Dashboard;
