a5069792efd8a6a5ba0bace9ed68df298fd18d6d
Emerald-A <adegbitedavid01@gmail.com>
2024-12-12 11:39:51 +0100

    fix(participant): fixed individual invite error

--- src/api/Participants.tsx
+++ src/api/Participants.tsx
@@ -10,7 +10,17 @@ class Participant {
   }
 
   inviteIndividualParticipant(cohortId: string, data: any) {
-    return this.client.post(`/cohort/${cohortId}/add-participants`, data);
+    return this.client.put(`/cohort/${cohortId}/add-participants`, data);
+  }
+
+  inviteGroupParticipant(cohortId: string, trackId: string, data: any) {
+    return this.client.put(`/cohort/${cohortId}/upload-participants-csv?track=${trackId}`, {
+      file: data
+    }, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    });
   }
 
 }
