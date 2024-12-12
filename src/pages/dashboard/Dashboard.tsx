c466702152fac753bd518bd4a912af3cff1b5715
Austin Asoluka <stracool9@gmail.com>
2024-12-12 13:06:09 +0100

    fetch programs from dashboard template

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -3,42 +3,9 @@ import "./index.css";
 import SetupProgram from "../../components/organisms/dashboard/SetupProgram/SetupProgram";
 import DashBoard from "../../components/organisms/dashboard/MainDashboard/DashBoard";
 import { ProgramContext } from "../../context/programs/ProgramContext";
-import { Skeleton } from "antd";
-import { useQuery } from "@tanstack/react-query";
-import api from "../../api/axios";
 
 const Dashboard = () => {
-  const { dispatch, programs } = useContext(ProgramContext);
-
-  const { isPending, isError } = useQuery({
-    queryKey: ["participants"],
-    queryFn: async () => {
-      const response = await api.program.getPrograms();
-      if (response.status === 200) {
-        dispatch({ type: "PROGRAMS", payload: response.data.data });
-      }
-      if (response.data.data?.length > 0) {
-        dispatch({ type: "ACTIVE_PROGRAM", payload: response.data.data[0] });
-      }
-      return response;
-    },
-  });
-
-  if (isPending) {
-    return <Skeleton loading />;
-  }
-
-  if (isError) {
-    return (
-      <div>
-        <div>
-          <h3>Err...</h3>
-          <p>Something went wrong ...</p>
-        </div>
-        <Skeleton loading avatar active paragraph title />
-      </div>
-    );
-  }
+  const { programs } = useContext(ProgramContext);
 
   return <>{programs[0] ? <DashBoard /> : <SetupProgram />}</>;
 };
--- src/pages/dashboard/settings/PermissionsSettings.tsx
+++ src/pages/dashboard/settings/PermissionsSettings.tsx
@@ -1,11 +1,9 @@
-import React, { useState } from 'react'
-import Table from '../../../components/organisms/dashboard/participants/Table'
-import api from '../../../api/axios';
-import { useQuery } from '@tanstack/react-query';
-import Button from '../../../components/atoms/Button';
-import { FiPlus } from 'react-icons/fi';
-import SearchInput from '../../../components/atoms/inputs/SearchInput';
-import GroupButton from '../../../components/atoms/Button/GroupButton';
+import React, { useState } from "react";
+import Button from "../../../components/atoms/Button";
+import { FiPlus } from "react-icons/fi";
+import SearchInput from "../../../components/atoms/inputs/SearchInput";
+import GroupButton from "../../../components/atoms/Button/GroupButton";
+import Table from "../../../components/organisms/dashboard/participants/Table";
 
 const PermissionsSettings = () => {
   const [activeView, setActiveView] = useState("All");
@@ -21,44 +19,66 @@ const PermissionsSettings = () => {
 
   const handleButtonClick = (view: string) => {
     setActiveView(view);
-
   };
 
-
   const buttonOptions = [
-    { label: "All", onClick: () => handleButtonClick("All"), active: activeView === "All" },
-    { label: "Active", onClick: () => handleButtonClick("Active"), active: activeView === "Active" },
-    { label: "Inactive", onClick: () => handleButtonClick("Inactive"), active: activeView === "Inactive" },
+    {
+      label: "All",
+      onClick: () => handleButtonClick("All"),
+      active: activeView === "All",
+    },
+    {
+      label: "Active",
+      onClick: () => handleButtonClick("Active"),
+      active: activeView === "Active",
+    },
+    {
+      label: "Inactive",
+      onClick: () => handleButtonClick("Inactive"),
+      active: activeView === "Inactive",
+    },
   ];
 
   return (
     <div>
-      <div className='d-flex w-100 justify-content-between align-items-center pb-5'>
+      <div className="d-flex w-100 justify-content-between align-items-center pb-5">
         <h5>Admin Accounts</h5>
-        <Button action={() => { }} type="button" fill={false} outline='primary' gap={true} width={177} border={true}>
+        <Button
+          action={() => {}}
+          type="button"
+          fill={false}
+          outline="primary"
+          gap={true}
+          width={177}
+          border={true}
+        >
           <FiPlus />
           <span>Add New Admin</span>
         </Button>
       </div>
-      <div className='d-flex justify-content-between pb-4'>
-
-        <SearchInput id="" placeHolder='Search by name' onchange={() => { }} width={18} />
+      <div className="d-flex justify-content-between pb-4">
+        <SearchInput
+          id=""
+          placeHolder="Search by name"
+          onchange={() => {}}
+          width={18}
+        />
         <div>
           <button
             className="btn rounded-pill bg-secondary-450 manrope-500 primary-950"
-            onClick={() => { }}
+            onClick={() => {}}
           >
             All
           </button>
           <button
             className="btn rounded-pill border-secondary manrope-500 dark-400"
-            onClick={() => { }}
+            onClick={() => {}}
           >
             Active
           </button>
           <button
             className="btn rounded-pill border-secondary manrope-500 dark-400"
-            onClick={() => { }}
+            onClick={() => {}}
           >
             Inactive
           </button>
@@ -67,11 +87,9 @@ const PermissionsSettings = () => {
         <GroupButton buttons={buttonOptions} />
       </div>
       <Table header={header} body={[]} />
-
     </div>
     // </div>
+  );
+};
 
-  )
-}
-
-export default PermissionsSettings
+export default PermissionsSettings;
