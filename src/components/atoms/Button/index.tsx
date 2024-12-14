d97a5e53022da83d10d734f3326f346846bd7986
Emerald-A <adegbitedavid01@gmail.com>
2024-12-14 08:18:26 +0100

    Drag and drop and customize program updated

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -2,46 +2,57 @@ import React from 'react'
 import { IButton } from '../../../@types/button.interface'
 import '../style.css'
 
-const Button: React.FC<IButton> = ({ 
-  children, 
-  type, 
-  action, 
-  fill, 
-  outline, 
-  border, 
-  gap, 
-  width, 
-  disabled, 
-  loading, 
-  outlineColor, 
-  customStyle 
+const Button: React.FC<IButton> = ({
+  children,
+  type,
+  action,
+  fill,
+  outline,
+  border,
+  gap,
+  width,
+  disabled,
+  loading,
+  outlineColor,
+  customStyle
 }) => {
-  
+
   return (
     <>
-      <button type={type}
+      <button
+        type={type}
         onClick={action}
         className={`
         ${fill ? 'fill' : 'outline'} 
         ${outline === 'white' ? 'border-white text-white' : outline === 'primary' ? 'primary-600' : outline === 'primary-700' ? 'primary-700' : outline === 'outlineColor' ? `${outlineColor}` : ''} 
         ${border ? '' : 'border-none'} 
         ${gap && 'gap'} 
-        button rounded-pill manrope-700 fs-button ${width ? '' : 'w-100'} d-flex align-items-center justify-content-center`}
-        style={
-          {
-            borderColor: outline === 'primary'
-            ? 'var(--primary-600)'
-            : outline === 'outlineColor'
-            ? `var(--${outlineColor})`
-            : '',
-            width: `${width}px`,
-            ...customStyle,
-          }
-        }
+        button rounded-pill manrope-700 fs-button ${width ? '' : 'w-100'} d-flex align-items-center justify-content-center
+        ${loading ? 'btn-loading' : ''} 
+      `}
+        style={{
+          borderColor:
+            outline === 'primary'
+              ? 'var(--primary-600)'
+              : outline === 'outlineColor'
+                ? `var(--${outlineColor})`
+                : '',
+          width: `${width}px`,
+          ...customStyle,
+        }}
         disabled={loading || disabled}
       >
-        {loading? <div className={`spinner-border ${fill && 'text-white'} ${outline && outline === 'white'? 'text-white': 'text-primary'}`}></div>: children}
+        {loading ? (
+          <div
+            className={`spinner-border ${fill && 'text-white'} ${outline && outline === 'white' ? 'text-white' : 'text-primary'}`}
+          ></div>
+        ) : disabled ? (
+          <div className='opacity-50'>Loading ...</div>
+        ) : (
+          children
+        )}
       </button>
+
     </>
   )
 }
